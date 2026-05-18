d434ce1ce70fafe47cb51d4b5c31876d46fc1e92
AyushDixit123 <ayushdixitworkmail@gmail.com>
2026-05-18 16:30:30 +0530

    handle concurrent clients

--- main.go
+++ main.go
@@ -19,31 +19,39 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	// access to tcp connection
-	//conn, err := l.Accept()
-	// conn is an object
-
-	// Send the response to the client
-	//	conn.Write([]byte("+PONG\r\n"))
-
-	buf := make([]byte, 1024) //make()-> does memory allocation similar to new()
-	//slice of byte is created to hold incoming data
-
 	for {
-		_, err = conn.Read(buf)
 
+		conn, err := l.Accept()
 		if err != nil {
-			break // sender has stopped sending commands
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+			break
 		}
 
-		conn.Write([]byte("+PONG\r\n")) //Redis client libraries (especially in languages like Go) use a slice of bytes ([]byte) instead of standard strings because slices of bytes prevent memory allocations and reduce CPU overhead.
-		//also Network sockets read data directly into byte buffers. If the library returned a string, it would have to copy those bytes into a brand new string object in memory. Returning []byte allows the library to pass a direct pointer to the network data without copying it.
+		go func() {
+
+			// access to tcp connection
+			//conn, err := l.Accept()
+			// conn is an object
+
+			// Send the response to the client
+			//	conn.Write([]byte("+PONG\r\n"))
+
+			buf := make([]byte, 1024) //make()-> does memory allocation similar to new()
+			//slice of byte is created to hold incoming data
+
+			for {
+				_, err = conn.Read(buf)
+
+				if err != nil {
+					break // sender has stopped sending commands
+				}
+
+				conn.Write([]byte("+PONG\r\n")) //Redis client libraries (especially in languages like Go) use a slice of bytes ([]byte) instead of standard strings because slices of bytes prevent memory allocations and reduce CPU overhead.
+				//also Network sockets read data directly into byte buffers. If the library returned a string, it would have to copy those bytes into a brand new string object in memory. Returning []byte allows the library to pass a direct pointer to the network data without copying it.
+			}
+		}()
+
 	}
 
 }
